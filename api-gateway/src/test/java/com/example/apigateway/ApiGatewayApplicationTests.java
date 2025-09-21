512c4076b43281069610adfd45fa6a32955a33a5
anuragvasubharti <anuragvasubharti@gmail.com>
2025-09-22 00:52:28 +0530

    Added

---
+++ api-gateway/src/test/java/com/example/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.apigateway;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	@Disabled // Disable the test temporarily
+	void contextLoads() {
+	}
+}
